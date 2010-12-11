35ab2f125f0bcefc5090ff552386baef075c6975
Carl Boettiger <cboettig@gmail.com>
2010-12-10 16:45:32 -0800

    added smart hashtags, and can pass tags to general functions

--- R/report.R
+++ R/report.R
@@ -3,22 +3,22 @@
 
 
 ## Discover tags from project directory
-get_tags <- function(){
+smart_tags <- function(){
 }
 
 ## all-in-one reporting, uploads specified files or just tweets the data
-social_report <- function(files=NULL, comment="", mention=" "){
+social_report <- function(files=NULL, comment="", mention=" ", tags = "", guess_tags=FALSE){
 	log <- gitlog()
-	tags <- get_tags()
+	if(guess_tags) tags <- c(tags, smart_tags() )
 	flickr(files, tags=tags, description=log$commitID)
-	tweet(comment=comment, hashtags=tags, commitID=log$commitID, mention=mention)
+	tweet(comment=comment, tags=tags, commitID=log$commitID, mention=mention)
 }
 
 ## A function that can be wrapped around a plot command to autoreport it
-social_plot <- function(plotcmd, comment="", mention=" ", device=c("png"), ...){
+social_plot <- function(plotcmd, comment="", mention=" ", tags="", device=c("png"), guess_tags=FALSE, ...){
 	if(device == "png"){ png("autoplot.png", ...)}
 	else { printf("device type not recognized"); return(0) }
 	plotcmd
 	dev.off()
-	social_report(files="autoplot.png", comment=comment, mention=mention)
+	social_report(files="autoplot.png", comment=comment, mention=mention, tags=tags, guess_tags=guess_tags)
 }
