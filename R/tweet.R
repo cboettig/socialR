467eaa5aee12b97cd9fe6cf95d8a7e427cfe3291
Carl Boettiger <cboettig@gmail.com>
2010-12-10 18:23:17 -0800

    trouble with git branches

--- R/tweet.R
+++
@@ -1,9 +0,0 @@
-#twitter.R
-tweet <- function(comment="", tags="", commitID="", mention=NULL){
-	# uses RegExp to add hashtag (pound, #) symbol before each tag
-	hashtags <- gsub("(\\b\\w.)", "\\#\\1", tags, perl=TRUE)
-#	if(!is.null(mention)){
-#		system(paste("hpc-autotweets ' @", mention, " ", comment, " ", hashtags, commitID, "'", sep="")) 
-#	} else { system(paste("hpc-autotweets ' ", comment, " ", hashtags, commitID, "'", sep="")) 
-#	} 
-}
---
+++ R/twitter.R
@@ -0,0 +1,8 @@
+#twitter.R
+tweet <- function(comment="", tags="", commitID="", mention=NULL){
+	# uses RegExp to add hashtag (pound, #) symbol before each tag
+	hashtags <- gsub("(\\b\\w.)",    "\\#\\1", tags, perl=TRUE)
+	if(!is.null(mention)){ system(paste("hpc-autotweets \" @", mention, " ", comment, " ", hashtags, commitID, "\"", sep="")) }
+	else {system(paste("hpc-autotweets \" ", comment, " ", hashtags, commitID, "\"", sep="")) } 
+}
+
