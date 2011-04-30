c2a89b756e45964091c548b17b0e1c5db4f391fa
Carl Boettiger <cboettig@gmail.com>
2011-04-29 17:26:48 -0700

    autocommit

--- R/report.R
+++ R/report.R
@@ -25,7 +25,7 @@ social_report <- function(files=NULL, comment=" ", mention=NULL, tags = "", gues
 	}
 
 	if(save){
-    env <- parent.env(environment())
+    env <- parent(parent.env(environment())) # If this function is being called by social_plot, this is true?
     save(list=ls(env), file=paste(flickr_id, ".Rdat", sep=""))
 		print(paste("datafile saved as ", flickr_id, ".Rdat", sep=""))
 	}
