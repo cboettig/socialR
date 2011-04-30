1711bdd05eadca9624abd8d5b729f04f7e9ef757
Carl Boettiger <cboettig@gmail.com>
2011-04-29 17:22:31 -0700

    autocommit

--- R/report.R
+++ R/report.R
@@ -4,20 +4,13 @@
 ## all-in-one reporting, uploads specified files or just tweets the data
 social_report <- function(files=NULL, comment=" ", mention=NULL, tags = "", guess_tags=FALSE, commit=FALSE, gituser="cboettig", flickruser="cboettig", urls=FALSE, public=TRUE, save=TRUE){
 
-	if(commit){
+	if(commit)
 		gitcommit()
-	}
-
 	log <- gitlog()
-
-	if(guess_tags){
+	if(guess_tags)
 		tags <- paste(tags, smart_tags() )
-	}
-
-	if(!is.null(files)){ 
+	if(!is.null(files))
 		tags <- paste(tags, files)
-	}
-
 	if(urls){
 		# grab the git url
 		giturl <- git_url(user=gituser) 
@@ -32,28 +25,40 @@ social_report <- function(files=NULL, comment=" ", mention=NULL, tags = "", gues
 	}
 
 	if(save){
-		save(list=ls(globalenv()), file=paste(flickr_id, ".Rdat", sep=""))
+    env <- parent.env(environment)
+    save(list=ls(env), file=paste(flickr_id, ".Rdat", sep=""))
 		print(paste("datafile saved as ", flickr_id, ".Rdat", sep=""))
 	}
 	flickr_id
 }
 
 ## A function that can be wrapped around a plot command to autoreport it
-social_plot <- function(plotcmd, file=NULL, comment="", mention=NULL, tags="", device=c("png"), guess_tags=FALSE, commit=TRUE, gituser="cboettig", flickruser="cboettig", public=TRUE, save=TRUE, ...){
+social_plot <- function(plotcmd, file=NULL, comment="", mention=NULL, tags="",
+                        device=c("png"), guess_tags=FALSE, commit=TRUE, 
+                        gituser="cboettig", flickruser="cboettig", public=TRUE,
+                        save=TRUE, ...){
 	if(is.null(file)){
 		if(device == "png"){ 
 			png("autoplot.png", ...)
 			file <- "autoplot.png"
 		}
-		else { printf("device type not recognized"); return(0) }
-
+		else { 
+      printf("device type not recognized") 
+      return(0) 
+    }
 	} else {
-		if(device == "png"){ png(file, ...)}
-		else { printf("device type not recognized"); return(0) }
+		if(device == "png"){
+      png(file, ...)
+    } else { 
+      printf("device type not recognized") 
+      return(0) 
+    }
 	}
 	plotcmd
 	dev.off()
-	social_report(files=file, comment=comment, mention=mention, tags=tags, guess_tags=guess_tags, commit=TRUE, flickruser=flickruser, gituser=gituser, public=public, save=save)
+	social_report(files=file, comment=comment, mention=mention, tags=tags,
+                guess_tags=guess_tags, commit=TRUE, flickruser=flickruser,
+                gituser=gituser, public=public, save=save)
 }
 
 
