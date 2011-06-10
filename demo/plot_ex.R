f7254fca0a99bf846f5e4c0ceb7df6e1c2e9aa84
Carl Boettiger <cboettig@gmail.com>
2011-06-09 17:23:37 -0700

    autocommit

--- demo/plot_ex.R
+++ demo/plot_ex.R
@@ -1,13 +1,12 @@
 #plot_ex.R
-# Pass an expression to social_plot
+# Pass a plot expression to social_plot
 require(socialR)
 
+exp <- {
+  plot(rnorm(10), rnorm(10))
+  curve(1*x, add=T)
+}
 
-
-
-social_plot( {
-plot(rnorm(10), rnorm(10))
-curve(1*x, add=T)
-}) 
+social_plot(exp) 
 
 
