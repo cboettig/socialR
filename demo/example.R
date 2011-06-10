bfc2484ff41f6b98fc4bfa313d87b9d19383b8ac
Carl Boettiger <cboettig@gmail.com>
2011-06-10 11:19:58 -0700

    example.R  autocommit

--- demo/example.R
+++ demo/example.R
@@ -1,22 +1,14 @@
 #####  socialR header info ##### 
 require(socialR)
-gitcommit(script)     # Must commmit at start!
 script <- "example.R" # Must specify the script name! 
+gitcommit(script)     # Must commmit at start!
 
-# specify the repository and directory information.  optional 
-# These values will be guessed correctly only if I'm the user and run one directory up.  
-gitopts = list(user = "cboettig", dir = "demo", repo = "socialR")
+### Optional but a good idea:  
+gitopts = list(user = "cboettig", dir = "demo", repo = "socialR") #almost requisite, if not me running in /demo
+on.exit(system("git push")) #  Need for git links.  May prompt for pw, so wait till end.  
+tags <- "test"  ## optional. Nice but establish a few standard ones only
+options(error=tweet_errors(script, gitopts, tags))## tweet on error
 
-# Optionally, at the end we can push so that the git links work
-# could do this earlier but don't want to hang,  Ideally this should check if origin is current
-# this may prompt for a password at the terminal, even in batch mode
-on.exit(system("git push")) 
-
-## tags, optional but nice
-tags <- "test"
-
-## Optionally, we can use twitter for error tracking:
-options(error=tweet_errors(script, gitopts, tags))
 
 
 # code code code .....
