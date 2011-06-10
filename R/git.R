c4d8f582706ced575d1e57d740b3813a9479cbd1
Carl Boettiger <cboettig@gmail.com>
2011-06-09 19:04:26 -0700

    updated git.R fns

--- R/git.R
+++ R/git.R
@@ -1,9 +1,21 @@
 #git.R
 
+
+
+## link to commit diffs
+#https://github.com/cboettig/socialR/commit/1034380c2ac2258f92855e753dd1a23bca9b028b
+
+## link to version
+#https://github.com/cboettig/socialR/blob/1034380c2ac2258f92855e753dd1a23bca9b028b/demo/plot_ex.R
+
+## raw
+#https://raw.github.com/cboettig/socialR/f7254fca0a99bf846f5e4c0ceb7df6e1c2e9aa84/demo/plot_ex.R
+
+
+
 ## the commitID 
-gitlog <- function(remote=FALSE){
-	if(!remote){ log <- system("git log -n 1", intern=TRUE) }
-	else{ log <- system("git log -n 1 origin", intern=TRUE) }
+gitlog <- function(){
+	log <- system("git log -n 1", intern=TRUE) 
 	commitID <- log[1]
 	author <- log[2]
 	date <- log[3]
@@ -14,19 +26,39 @@ list(commitID = commitID, author=author, date=date, short_comment=short_comment)
 }
 
 # consider allowing R to check if a commit should be done
-gitcommit <- function(msg=""){
-	system(paste("git commit -a -m", "'", msg, "autocommit", "'"), intern=TRUE)
+gitcommit <- function(filename="", msg=""){
+  if(filename==""){
+  	system(paste("git commit -a -m", "'", msg, "autocommit", "'"), intern=TRUE)
+  } else {
+  	system(paste("git add", filename), intern=TRUE)
+  	system(paste("git commit -m", "'", filename, msg, "autocommit", "'"), intern=TRUE)
+  }
 }
 
-# get the url to the code on github
-git_url <- function(user="cboettig"){
-## get the repository name from the remote directory
-	remote <- system("git remote -v", intern=TRUE)[1]
-	repository <- gsub("[^/]+/([a-zA-Z]+)\\.git.*", "\\1", remote)
-## get the commitid
+
+
+
+# get the url to the code on github.  Code still needs to be pushed before link will work!
+# note that default guesses may fail, specify all options!
+git_url <- function(filename, user="cboettig", repository=NULL, dir="NULL",
+                    raw=FALSE, diff=FALSE){
+
+  gitcommit(filename) 
+
+  if(is.null(repository)) ## guesses is one directory up
+    repository <-gsub(".*/(.*)/.*$", "\\1", getwd())
+  if(is.null(dir)) ## guess the current directory
+    dir <-gsub(".*/(.*)$", "\\1", getwd())
 	log <- gitlog()
 	id <- gsub("\\s", "/", log$commitID)
-	paste("https://github.com/", user, "/", repository, "/", id, sep="") 
+  domain <- "https://github.com"
+
+  if(raw)
+    out <- paste("https://raw.github.com", user, repository, id, dir, filename, sep="/") 
+  else if(diff)
+    out <- paste(domain, user, repository, "commit", id, sep="/") 
+  else 
+    out <- paste(domain, user, repository, "blob", id, dir, filename, sep="/") 
 }
 
 
