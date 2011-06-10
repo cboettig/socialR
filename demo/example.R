3f6ccaf2647fa8489521d0226d1a29d4216aa8dd
Carl Boettiger <cboettig@gmail.com>
2011-06-09 19:00:00 -0700

    example.R  autocommit

--- demo/example.R
+++ demo/example.R
@@ -2,16 +2,19 @@
 # Pass a plot expression to social_plot
 require(socialR)
 
-filename <- "plot_ex.R"
+filename <- "example.R"
 dir <- "demo"
 repo <- "socialR"
 user <- "cboettig"
 
-
-tweet(git_url(filename, user=user, repo=repo, dir=dir))
+gitaddr <- git_url(filename, user=user, repo=repo, dir=dir)
 #social_plot({
-#  plot(rnorm(10), rnorm(10))
-#  curve(1*x, add=T)
+
+png("test.png")
+  plot(rnorm(10), rnorm(10))
+  curve(1*x, add=T)
+dev.off()
 #})
 
-
+flickr("test.png", description=gitaddr)
+syste("git push")
