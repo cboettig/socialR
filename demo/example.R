2490ec949a3d8eba585fbe38ad688ba5c3f7c037
Carl Boettiger <cboettig@gmail.com>
2011-06-10 12:56:09 -0700

    example.R  autocommit

--- demo/example.R
+++ demo/example.R
@@ -3,23 +3,25 @@ require(socialR)
 script <- "example.R" # Must specify the script name! 
 gitcommit(script)     # Must commmit at start!
 
-### Optional but a good idea:  
-gitopts = list(user = "cboettig", dir = "demo", repo = "socialR") #almost requisite, if not me running in /demo
-on.exit(system("git push")) #  Need for git links.  May prompt for pw, so wait till end.  
-tags <- "test"  ## optional. Nice but establish a few standard ones only
-
+##### Optional but a good idea ##### 
+#almost requisite, if not me running "repo"/"dir"
+gitopts = list(user = "cboettig", dir = "demo", repo = "socialR") 
+on.exit(system("git push")) #  For git links.  May prompt for pw,
+tags <- "test"  ## multiple possible: space, delim, multiple items, etc.  
 tweet_errors(script, gitopts, tags)  ## tweet on error
 
-
-
 # code code code .....
+
+# we save some plots ....
 png("test.png")
   hist(rnorm(100))
 dev.off()
 
 
-# and we have some results to commit...
+# and here's the call to upload, link, save data, and tweet. 
+# first entry can be a space-delim list of images "image1.png image2.png"
 upload("test.png", script=script, gitopts=gitopts, tags="test", tweet=T) 
 
-
+# and create and error
+hist(mydata)
 
